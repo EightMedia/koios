104b507271b903d33cb16e83d2b4084da00f6c48
hylmar <hylmar@eight.nl>
2020-09-09 16:39:53 +0200

    use dependency-tree for js and scss children

--- source/utils/get-children.js
+++ source/utils/get-children.js
@@ -1,23 +1,14 @@
 const pugDependencies = require("pug-dependencies");
-const paperwork = require("precinct").paperwork;
+const depTree = require("dependency-tree");
 const path = require("path");
 
 const getChildren = {
   pug: (source) => pugDependencies(source),
-  scss: (source) => paperwork(source)
-    .map(child => path.resolve(path.dirname(source), child) + ".scss"),
-    js: (source) => paperwork(source)
-    .filter(child => child[0] === ".")
-    .map(child => {
-      const ext = path.extname(child);
-      if (!ext) child += ".js";
-      return path.resolve(path.dirname(source), child);
-    }),
+  scss: (source) => depTree.toList({ filename: source, directory: path.dirname(source) }),
+  js: (source) => depTree.toList({ filename: source, directory: path.dirname(source), filter: path => path.indexOf("node_modules") === -1  }),
 }
 
 module.exports = function(source) {
   const ext = path.extname(source).substr(1);
-  const c = getChildren[ext](source);
-  console.log(c);
-  return c;
+  return getChildren[ext](source);
 }
