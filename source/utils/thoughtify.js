d5a4b58532886f3d1d1e198aac4fd042c712027c
hylmar <hylmar@gmail.com>
2021-03-03 20:28:35 +0100

    style error in thoughtify

--- source/utils/thoughtify.js
+++ source/utils/thoughtify.js
@@ -39,7 +39,7 @@ module.exports = ({ source, destination, changed, children, data }) => ({
     if (msg instanceof Error) {
       msg = {
         msg: pathDiff(process.cwd(), this.source),
-        errors: [msg.message]
+        errors: [chalk.grey(msg.message)]
       }
     }
     this.log = this.log || typeof msg === "string" ? { type: "error", msg } : Object.assign(msg, { type: "error" });
