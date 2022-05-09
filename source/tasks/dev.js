7bfd9862ff5a7cdbea47b8741b31790119968286
hylmar <hylmar@gmail.com>
2022-05-09 16:26:39 +0200

    upgrade to http2

--- source/tasks/dev.js
+++ source/tasks/dev.js
@@ -1,48 +1,43 @@
 import config from "../config.js";
 import run from "../run.js";
 import path from "path";
-import browserSync from "browser-sync";
 import chokidar from "chokidar";
+import liveServer from "live-server";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+import fs from "node:fs";
 
-const bs = browserSync.create("localdev");
-const argv = yargs(hideBin(process.argv)).argv
-
+const argv = yargs(hideBin(process.argv)).argv;
 const verbose = argv.v || argv.verbose || false;
-
 
 /**
  * Entry point
  */
 
 export default function () {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     const chokidarOptions = {
       awaitWriteFinish: {
         stabilityThreshold: 500,
-        pollInterval: 100
-      }
+        pollInterval: 100,
+      },
     };
 
     /**
-     * Run BrowserSync
+     * Run Local Server
      */
 
-    bs.init({
-      server: {
-        baseDir: config.paths.roots.to,
-        directory: true,
-      },
-      files: [
-        path.join(config.paths.roots.to, "**/*.css"),
-        path.join(config.paths.roots.to, "**/*.js"),
-        path.join(config.paths.roots.to, "**/*.html"),
-      ],
-      watchOptions: chokidarOptions,
-      notify: false,
+    liveServer.start({
       port: 8000,
-      open: false
+      root: config.paths.roots.to,
+      logLevel: 0,
+      open: false,
+      ignore: "koios",
+      httpsModule: "spdy",
+      https: {
+        key: fs.readFileSync(config.https.key),
+        cert: fs.readFileSync(config.https.cert),
+      },
     });
 
     /**
@@ -53,21 +48,21 @@ export default function () {
       [
         `${config.paths.roots.from}/**/*.scss`,
         `${config.paths.roots.from}/**/*.js`,
-        `${config.paths.roots.from}/**/*.pug`
+        `${config.paths.roots.from}/**/*.pug`,
       ],
       chokidarOptions
     );
 
-    watcher.on("change", function(file) {
+    watcher.on("change", function (file) {
       const tasks = {
         scss: "styles",
         js: "scripts",
-        pug: ["parts", "pages"]
+        pug: ["parts", "pages"],
       };
 
       const ext = path.extname(file).substr(1);
 
-      run({ tasks: tasks[ext], file, verbose }).catch(err => {
+      run({ tasks: tasks[ext], file, verbose }).catch((err) => {
         reject(err);
       });
     });
